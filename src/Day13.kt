bd8f32a398437c8e16cf0d5b0774edbaaf4386ad
Valentin Slawicek <valentin@slawicek.com>
2024-12-14 15:50:49 +0100

    Day 13 Part 2 (not working yet)

--- src/Day13.kt
+++ src/Day13.kt
@@ -1,6 +1,3 @@
-import kotlin.math.max
-import kotlin.math.min
-
 class Button(x: Long, y: Long, val cost: Long) : Point(x, y)
 class Price(x: Long, y: Long) : Point(x, y)
 data class Machine(val buttons: List<Button>, var price: Price)
@@ -35,25 +32,41 @@ fun main() {
             val lowCostButton = buttons.minBy { it.cost * (it.x + it.y) }
             val highCostButton = buttons.single { it != lowCostButton }
 
-            val baseLowCostButtonCount = min(price.x / lowCostButton.x, price.y / lowCostButton.y)
+            // Cross the lines: https://de.wikipedia.org/wiki/Schnittpunkt
+            // x1 = y1 = 0
 
-            for (tick in baseLowCostButtonCount downTo max(0L, baseLowCostButtonCount - 1000)) {
-                var currentTokens = lowCostButton.cost * tick
-                var target = lowCostButton * tick
+            val x2 = lowCostButton.x
+            val y2 = lowCostButton.y
 
-                for (highCostButtonPresses in 0..1000) {
-                    currentTokens += highCostButton.cost
-                    target += highCostButton
-                    if (target.x >= price.x || target.y >= price.y) {
-                        break
-                    }
-                }
+            val x3 = price.x
+            val y3 = price.y
 
-                if (target.x == price.x && target.y == price.y) {
-                    totalTokens += currentTokens
-                    return@forEach
-                }
+            val x4 = price.x - highCostButton.x
+            val y4 = price.y - highCostButton.y
+
+            val denominator = (x2 * (y4 - y3) - y2 * (x4 - x3))
+            if (denominator == 0L) {
+                throw IllegalStateException("parallel button directions")
             }
+
+            val s_x_times_denominator = (x2 * (x3 * y4 - y3 * x4))
+            val s_y_times_denominator = (y2 * (x3 * y4 - y3 * x4))
+
+            val s_x = s_x_times_denominator / denominator
+            val s_y = s_y_times_denominator / denominator
+
+            // Check if we had whole numbers
+            if (s_x * denominator != s_x_times_denominator || s_y * denominator != s_y_times_denominator) {
+                return@forEach
+            }
+
+            if (s_x < 0 || s_x > price.x || s_y < 0 || s_y > price.y) {
+                return@forEach
+            }
+
+            val cost =
+                lowCostButton.cost * (s_x / lowCostButton.x) + highCostButton.cost * (price.y - s_y) / highCostButton.y
+            totalTokens += cost
         }
         return totalTokens
     }
@@ -63,11 +76,13 @@ fun main() {
             .also { check(it == 36250L, { "total tokens $it" }) }
     }
 
-    fun part2(input: List<String>): Long {
+    fun part2(): Long {
         return calculateTokens(
             readInput("Day13_test").toMachines()
                 .onEach { it.price = Price(it.price.x + 10000000000000L, it.price.y + 10000000000000L) })
+            .also { check(it < 85132304518943L, { "total tokens $it" }) }
+
     }
     part1().println()
-    //part2(input).println()
+    part2().println()
 }
